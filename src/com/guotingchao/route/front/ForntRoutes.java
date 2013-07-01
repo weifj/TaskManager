88562e8c9a37c703e47c5a2106e0e9757a412cd8
yuguiqi <fengyi426@163.com>
2013-07-01 19:50:48 +0800

    add login Routes

--- src/com/guotingchao/route/front/ForntRoutes.java
+++ src/com/guotingchao/route/front/ForntRoutes.java
@@ -3,13 +3,17 @@ package com.guotingchao.route.front;
 import com.guotingchao.controlle.front.IndexController;
 import com.guotingchao.controlle.front.LoginController;
 import com.guotingchao.controlle.front.ShowController;
+import com.guotingchao.controlle.front.TaskAddController;
 import com.guotingchao.controlle.front.TaskController;
+import com.guotingchao.controlle.front.ValidateController;
 import com.jfinal.config.Routes;
 
 public class ForntRoutes extends Routes {
 	
 	@Override
 	public void config() {
+		add("/addTask",TaskAddController.class);
+		add("/validate",ValidateController.class);
 		add("/login",LoginController.class);
 		add("/taskInfo/show",ShowController.class);
 		add("/taskInfo",TaskController.class);
