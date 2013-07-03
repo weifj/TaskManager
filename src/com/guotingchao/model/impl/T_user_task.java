e44b8cf313735862d2a05526fb89afe27bebd443
yuguiqi <fengyi426@163.com>
2013-07-03 23:21:16 +0800

    extends baseModel

--- src/com/guotingchao/model/impl/T_user_task.java
+++ src/com/guotingchao/model/impl/T_user_task.java
@@ -1,8 +1,8 @@
 package com.guotingchao.model.impl;
 
+import com.guotingchao.model.BaseModel;
 import com.guotingchao.model.ITaskUserkDao;
-import com.jfinal.plugin.activerecord.Model;
-public class T_user_task extends Model<T_user_task> implements ITaskUserkDao{
+public class T_user_task extends BaseModel<T_user_task> implements ITaskUserkDao{
 	private static final long serialVersionUID = 1L;
 	public static T_user_task taskUserDao = new T_user_task();
 	
