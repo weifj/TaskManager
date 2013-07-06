26bee357acd2bcda84570107c29b695ebd24b518
yuguiqi <fengyi426@163.com>
2013-07-06 15:31:28 +0800

    change route

--- src/com/guotingchao/route/front/ForntRoutes.java
+++ src/com/guotingchao/route/front/ForntRoutes.java
@@ -1,17 +1,13 @@
 package com.guotingchao.route.front;
 
 import com.guotingchao.controlle.front.IndexController;
-import com.guotingchao.controlle.front.LoginController;
 import com.guotingchao.controlle.front.ShowController;
-import com.guotingchao.controlle.front.TaskAddController;
 import com.jfinal.config.Routes;
 
 public class ForntRoutes extends Routes {
 	
 	@Override
 	public void config() {
-		add("/addTask",TaskAddController.class);
-		add("/login",LoginController.class);
 		add("/taskInfo/show",ShowController.class);
 		add("/",IndexController.class);
 	}
