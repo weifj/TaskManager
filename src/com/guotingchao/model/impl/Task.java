21ca3f296e71d1d523952708c57953e52858141f
yuguiqi <fengyi426@163.com>
2013-07-06 15:29:15 +0800

    delete unuse method

--- src/com/guotingchao/model/impl/Task.java
+++ src/com/guotingchao/model/impl/Task.java
@@ -7,18 +7,22 @@ import com.jfinal.plugin.activerecord.Model;
 public class Task extends Model<Task> implements ITaskDao{
 	private static final long serialVersionUID = 1L;
 	public static Task taskDao = new Task();
+	
+	@Override
+	public List<Task> findAllTaskList() {
+		
+		return Task.taskDao.find("select * from task ");
+	}
+	
 	@Override
 	public List<Task> findTaskListByType(int taskType) {
 		return Task.taskDao.find("select * from task where tasktype = ? ",taskType);
 	}
+	
 	@Override
 	public Task findTaskById(Long id) {
 		return Task.taskDao.findById(id);
 	}
-	@Override
-	public List<String> findUserListByTaskId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 	
 }
