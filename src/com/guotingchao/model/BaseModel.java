3141844ecb4e02ada79acca6e565c50694e671f7
yuguiqi <fengyi426@163.com>
2013-07-06 15:30:12 +0800

    udpate

--- src/com/guotingchao/model/BaseModel.java
+++ src/com/guotingchao/model/BaseModel.java
@@ -2,35 +2,43 @@ package com.guotingchao.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.jfinal.log.Log4jLogger;
+import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Model;
 
 
 public class BaseModel<Class> extends Model<BaseModel<Class>>{
+	Logger log = Log4jLogger.getLogger(BaseModel.class);
 	private static final long serialVersionUID = 1L;
 	public void batchSave(List<Model> modelList){
 		List<String> sqlList = new ArrayList<String>();
-		String[] temp=modelList.get(0).getClass().getName().split("\\.");String tableName=temp[temp.length-1];  //改
-		for(Model model : modelList){
-			StringBuffer sql = new StringBuffer("insert into "+tableName+"(" );
-			StringBuffer name= new StringBuffer() ,value = new StringBuffer();
-			
-			String[] attr_names = model.getAttrNames();int attr_names_length = attr_names.length;
-			String attx = ""; //前缀
-			for(int i = 0 ; i<attr_names_length;i++){
-				if(i>0){attx=",";}
-				String attr_key =attr_names[i]; 
-				name.append(attx+attr_key);
-				value.append(attx+model.get(attr_key));
+		try{
+			String[] temp=modelList.get(0).getClass().getName().split("\\.");String tableName=temp[temp.length-1];  //改
+			for(Model model : modelList){
+				StringBuffer sql = new StringBuffer("insert into "+tableName+"(" );
+				StringBuffer name= new StringBuffer() ,value = new StringBuffer();
 				
+				String[] attr_names = model.getAttrNames();int attr_names_length = attr_names.length;
+				String attx = ""; //前缀
+				for(int i = 0 ; i<attr_names_length;i++){
+					if(i>0){attx=",";}
+					String attr_key =attr_names[i]; 
+					name.append(attx+attr_key);
+					value.append(attx+model.get(attr_key));
+					
+				}
+				sql.append(name);
+				sql.append(") values(");
+				sql.append(value);
+				sql.append(")");
+				
+				sqlList.add(sql.toString());
 			}
-			sql.append(name);
-			sql.append(") values(");
-			sql.append(value);
-			sql.append(")");
-			
-			sqlList.add(sql.toString());
+			Db.batch(sqlList, sqlList.size());
+		}catch(NullPointerException e){
+			log.error(e.getMessage());
 		}
-		Db.batch(sqlList, sqlList.size());
+		
 	}
 }
