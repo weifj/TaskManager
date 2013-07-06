74a7cfedf17e88b7d82cbb96e4146e6dab3f5890
yuguiqi <fengyi426@163.com>
2013-07-06 15:30:44 +0800

    add method of find all task

--- src/com/guotingchao/model/ITaskDao.java
+++ src/com/guotingchao/model/ITaskDao.java
@@ -6,24 +6,25 @@ import com.guotingchao.model.impl.Task;
 
 public interface ITaskDao{
 	
+	/**
+	 * 获取所有任务
+	 * @return List
+	 */
+	public List<Task> findAllTaskList();
 	
 	/**
 	 * 通过任务类型获取任务列表
 	 * @param taskType
-	 * @return List<Task>
+	 * @return List
 	 */
 	public List<Task> findTaskListByType(int taskType);
+	
 	/**
 	 * 通过id获取task
 	 * @param id
 	 * @return task
 	 */
 	public Task findTaskById(Long id);
-	/**
-	 * 通过id获取执行任务人列表
-	 * @param id
-	 * @return List<User> 
-	 */
-	public List<String> findUserListByTaskId(Long id); 
+	
 	
 }
