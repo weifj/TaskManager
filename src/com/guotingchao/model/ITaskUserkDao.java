d66fd3ef5bf2664daac9c2660428936334e8626e
yuguiqi <fengyi426@163.com>
2013-07-14 22:20:41 +0800

    添加方法

--- src/com/guotingchao/model/ITaskUserkDao.java
+++ src/com/guotingchao/model/ITaskUserkDao.java
@@ -1,6 +1,29 @@
 package com.guotingchao.model;
 
+import java.util.List;
+
+import com.guotingchao.model.impl.T_user_task;
+
 
 public interface ITaskUserkDao{
+	/**
+	 * 通过 tid 获取对应的uid
+	 * @param tid
+	 * @return
+	 */
+	public String[] getUidArray(Long tid);
 	
+	/**
+	 * 通过 tid和uid 删除对象
+	 * @param tid
+	 * @param uid
+	 * @return
+	 */
+	public boolean deleteByTidAndUid(Long tid,Long uid);
+	/**
+	 * 通过uid获取有信息提示的T_user_task对象
+	 * @param uid
+	 * @return
+	 */
+	public List<BaseModel<T_user_task>> findMsgTaskByUid(Long uid);
 }
--- src/com/guotingchao/model/impl/T_user_task.java
+++ src/com/guotingchao/model/impl/T_user_task.java
@@ -1,9 +1,36 @@
 package com.guotingchao.model.impl;
 
+import java.util.List;
+
 import com.guotingchao.model.BaseModel;
 import com.guotingchao.model.ITaskUserkDao;
 public class T_user_task extends BaseModel<T_user_task> implements ITaskUserkDao{
 	private static final long serialVersionUID = 1L;
 	public static T_user_task taskUserDao = new T_user_task();
+	
+	@Override
+	public String[] getUidArray(Long tid) {
+		List<BaseModel<T_user_task>> list = taskUserDao.find("select uid from t_user_task where tid=?", tid);
+		int count = list.size();
+		String[] uid = new String[count];
+		for(int i = 0;i < count; i++){
+			uid[i] = list.get(i).getLong("uid")+"";
+		}
+		return uid;
+	}
+
+	@Override
+	public boolean deleteByTidAndUid(Long tid, Long uid) {
+		Long id = taskUserDao.find("select id from t_user_task where tid=? and uid=?", tid,uid)
+				.get(0).getLong("id");
+		return taskUserDao.deleteById(id);
+	}
+
+	@Override
+	public List<BaseModel<T_user_task>> findMsgTaskByUid(Long uid) {
+		
+		return taskUserDao.find("select * from t_user_task where messageType=0 and uid=? ", uid);
+	
+	}
 	
 }
