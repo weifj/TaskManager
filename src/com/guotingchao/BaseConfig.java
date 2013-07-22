32a183fbe4822a49c52b99e5a2033985d91c323a
yuguiqi <fengyi426@163.com>
2013-07-22 23:03:39 +0800

    修改 加入配置文件

--- src/com/guotingchao/BaseConfig.java
+++ src/com/guotingchao/BaseConfig.java
@@ -1,5 +1,12 @@
 package com.guotingchao;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import sun.util.logging.resources.logging;
+
+
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.wall.WallFilter;
 import com.guotingchao.model.impl.T_user_task;
@@ -13,13 +20,25 @@ import com.jfinal.config.Interceptors;
 import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
+import com.jfinal.log.Log4jLogger;
+import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.render.JspRender;
 import com.jfinal.render.ViewType;
 
 public class BaseConfig extends JFinalConfig {
+	public static Logger log  = Log4jLogger.getLogger(BaseConfig.class);
 
+	static Properties prop = new Properties();
+	static{
+		InputStream in = BaseConfig.class.getClassLoader().getResourceAsStream("config.properties");
+		try{
+			prop.load(in);
+		}catch(IOException e){
+			log.error(e.getMessage());
+		}
+	}
 	@Override
 	public void configConstant(Constants me) {
 		me.setDevMode(true);
@@ -52,7 +71,7 @@ public class BaseConfig extends JFinalConfig {
 	@Override
 	public void configPlugin(Plugins me) {
 		DruidPlugin dp = new DruidPlugin(
-				"jdbc:MySQL://localhost:3306/TaskManager", "root", "root");
+				prop.getProperty("dataBase_address"), prop.getProperty("dataBase_user"), prop.getProperty("dataBase_pass"));
 		dp.addFilter(new StatFilter());
 		WallFilter wall = new WallFilter();
 		wall.setDbType("mysql");
