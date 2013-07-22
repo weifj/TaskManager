61eeea11bcbc843596126472ebb2e5cb29710bba
yuguiqi <fengyi426@163.com>
2013-07-22 23:01:26 +0800

    分页

---
+++ src/com/guotingchao/PaginationContext.java
@@ -0,0 +1,16 @@
+package com.guotingchao;
+
+public class PaginationContext {
+
+	public static int pageNo;
+
+	public static int getPageNo() {
+		return pageNo;
+	}
+
+	public static void setPageNo(int pageNo) {
+		PaginationContext.pageNo = pageNo;
+	}
+
+
+}
